07361cade9e81ce7505854aad3267b167f5563ef
BejenaruMatei <matei-andrei.bejenaru@student.tuiasi.ro>
2024-04-08 12:46:32 +0300

    Added EventHandler and Events - Bug fixing

--- src/main/EventHandler.java
+++ src/main/EventHandler.java
@@ -1,63 +1,90 @@
 package main;
-import java.awt.Rectangle;
+import java.awt.*;
 
 public class EventHandler {
     GamePanel gp;
-    Rectangle eventRect;
-    int eventRectDefaultX, eventRectDefaultY;
+    EventRect[][] eventRect;
+
+    int previousEventX, previousEventY;
+    boolean canTouchEvent = true;
 
     public EventHandler(GamePanel gp) {
         this.gp = gp;
 
-        eventRect = new Rectangle();
-        eventRect.x = 23;
-        eventRect.y = 23;
-        eventRect.width = 2;
-        eventRect.height = 2;
-        eventRectDefaultX = eventRect.x;
-        eventRectDefaultY = eventRect.y;
+        eventRect = new EventRect[gp.maxWorldColumn][gp.maxWorldRow];
+
+        int column = 0;
+        int row = 0;
+        while(column < gp.maxWorldColumn && row < gp.maxWorldRow) {
+            eventRect[column][row] = new EventRect();
+            eventRect[column][row].x = 23;
+            eventRect[column][row].y = 23;
+            eventRect[column][row].width = 2;
+            eventRect[column][row].height = 2;
+            eventRect[column][row].eventRectDefaultX = eventRect[column][row].x;
+            eventRect[column][row].eventRectDefaultY = eventRect[column][row].y;
+
+            column++;
+            if(column == gp.maxWorldColumn) {
+                column = 0;
+                row++;
+            }
+        }
     }
 
     public void checkEvent() {
-        if(hit(23, 29, "any")) {
-            giveDamage(gp.dialogState);
+
+        int xDistance = Math.abs(gp.player.worldx - previousEventX);
+        int yDistance = Math.abs(gp.player.worldy - previousEventY);
+        int distance = Math.max(xDistance, yDistance);
+        if(distance > gp.tileSize) {
+            canTouchEvent = true;
         }
-        if(hit(25, 40, "any")) {
-            heal(gp.dialogState);
+        if(canTouchEvent) {
+            if(hit(22, 30, "any")) {
+                giveDamage(22, 30, gp.dialogState);
+            }
+            if(hit(21, 30, "any")) {
+                heal(25, 40, gp.dialogState);
+            }
         }
     }
 
-    public void giveDamage(int gameState) {
+    public void giveDamage(int column, int row, int gameState) {
         gp.gameState = gameState;
         gp.ui.dialogText = "Ai luat damage!";
         gp.player.life--;
+//        eventRect[column][row].eventDone = true;
+        canTouchEvent = false;
     }
 
-    public void heal(int gameState) {
+    public void heal(int column, int row, int gameState) {
         gp.gameState = gameState;
         gp.ui.dialogText = "Ai luat heal!";
         gp.player.life = gp.player.maxLife;
     }
 
-    public boolean hit(int eventColumn, int eventRow, String direction) {
+    public boolean hit(int column, int row, String direction) {
         boolean hit = false;
 
         gp.player.solidArea.x = gp.player.worldx + gp.player.solidArea.x;
         gp.player.solidArea.y = gp.player.worldy + gp.player.solidArea.y;
-        eventRect.x = eventColumn * gp.tileSize + eventRect.x;
-        eventRect.y = eventColumn * gp.tileSize + eventRect.y;
+        eventRect[column][row].x = column * gp.tileSize + eventRect[column][row].x;
+        eventRect[column][row].y = row * gp.tileSize + eventRect[column][row].y;
 
-        if(gp.player.solidArea.intersects(eventRect)) {
+        if(gp.player.solidArea.intersects(eventRect[column][row]) && !eventRect[column][row].eventDone) {
             if(gp.player.direction.contentEquals(direction) || direction.contentEquals("any")) {
                 hit = true;
+
+                previousEventX = gp.player.worldx;
+                previousEventY = gp.player.worldy;
             }
         }
 
         gp.player.solidArea.x = gp.player.solidAreaDefaultX;
         gp.player.solidArea.y = gp.player.solidAreaDefaultY;
-        eventRect.x = eventRectDefaultX;
-        eventRect.y = eventRectDefaultY;
-
+        eventRect[column][row].x = eventRect[column][row].eventRectDefaultX;
+        eventRect[column][row].y = eventRect[column][row].eventRectDefaultY;
 
         return hit;
     }
