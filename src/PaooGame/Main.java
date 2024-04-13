17a0c2650cc84295b664baa66f81d18f0acb1397
BejenaruMatei <matei-andrei.bejenaru@student.tuiasi.ro>
2024-04-13 15:23:42 +0300

    New Structure 8)

--- src/PaooGame/Main.java
+++ src/PaooGame/Main.java
@@ -6,8 +6,10 @@ import static java.lang.System.getProperty;
 
 public class Main {
     public static void main(String[] args) {
+        // Game Start
         Game gamePanel = new Game();
         gamePanel.setupGame();
         gamePanel.startGame();
+        // End
     }
 }
