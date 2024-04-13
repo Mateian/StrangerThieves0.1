ca28c84a4b715a7b5fa00556b03741faf1898bc4
BejenaruMatei <matei-andrei.bejenaru@student.tuiasi.ro>
2024-04-13 14:16:41 +0300

    Cleaning

--- src/PaooGame/main/AssetSetter.java
+++ src/PaooGame/main/AssetSetter.java
@@ -15,41 +15,9 @@ public class AssetSetter {
     }
 
     public void setObject() {
-        gp.obj[0] = new OBJ_Key(gp);
-        gp.obj[0].worldX = 23 * gp.tileSize;
-        gp.obj[0].worldY = 7 * gp.tileSize;
-
-        gp.obj[1] = new OBJ_Key(gp);
-        gp.obj[1].worldX = 23 * gp.tileSize;
-        gp.obj[1].worldY = 40 * gp.tileSize;
-
-        gp.obj[2] = new OBJ_Key(gp);
-        gp.obj[2].worldX = 38 * gp.tileSize;
-        gp.obj[2].worldY = 8 * gp.tileSize;
-
-        gp.obj[3] = new OBJ_Door(gp);
-        gp.obj[3].worldX = 10 * gp.tileSize;
-        gp.obj[3].worldY = 11 * gp.tileSize;
-
-        gp.obj[4] = new OBJ_Door(gp);
-        gp.obj[4].worldX = 8 * gp.tileSize;
-        gp.obj[4].worldY = 28 * gp.tileSize;
-
-        gp.obj[5] = new OBJ_Door(gp);
-        gp.obj[5].worldX = 12 * gp.tileSize;
-        gp.obj[5].worldY = 22 * gp.tileSize;
-
-        gp.obj[6] = new OBJ_Chest(gp);
-        gp.obj[6].worldX = 10 * gp.tileSize;
-        gp.obj[6].worldY = 7 * gp.tileSize;
-
-        gp.obj[7] = new OBJ_Boots(gp);
-        gp.obj[7].worldX = 37 * gp.tileSize;
-        gp.obj[7].worldY = 42 * gp.tileSize;
-
-        gp.obj[8] = new OBJ_Skargun(gp);
-        gp.obj[8].worldX = 22 * gp.tileSize;
-        gp.obj[8].worldY = 30 * gp.tileSize;
+        gp.obj[0] = new OBJ_Skargun(gp);
+        gp.obj[0].worldX = 22 * gp.tileSize;
+        gp.obj[0].worldY = 30 * gp.tileSize;
     }
 
     public void setNPC() {
@@ -68,13 +36,5 @@ public class AssetSetter {
                 k++;
             }
         }
-//
-//        gp.mst[1] = new MST_Enemy(gp);
-//        gp.mst[1].worldx = gp.tileSize * 24;
-//        gp.mst[1].worldy = gp.tileSize * 25;
-//
-//        gp.mst[2] = new MST_Enemy(gp);
-//        gp.mst[2].worldx = gp.tileSize * 23;
-//        gp.mst[2].worldy = gp.tileSize * 27;
     }
 }
